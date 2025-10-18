4fa072ecb9ae903b590f946b67ff5d00f2cbe454
Jayden Chun <jayden.h.chun@gmail.com>
2025-10-18 01:04:03 -0500

    cleaned up SettingsScreen.tsx + changed the text to make it a true settings page

--- app/screens/SettingsScreen.tsx
+++ app/screens/SettingsScreen.tsx
@@ -77,32 +77,17 @@ export const SettingsScreen: FC<DemoTabScreenProps<"Settings">> = function Setti
       safeAreaEdges={["top"]}
       contentContainerStyle={[$styles.container, themed($container)]}
     >
-      <Text
-        style={themed($reportBugsLink)}
-        tx="demoDebugScreen:reportBugs"
-        onPress={() => openLinkInBrowser("https://github.com/infinitered/ignite/issues")}
-      />
-
-      <Text style={themed($title)} preset="heading" tx="demoDebugScreen:title" />
-      <Text preset="bold">Current system theme: {colorScheme}</Text>
-      <Text preset="bold">Current app theme: {themeContext}</Text>
-      <Button onPress={resetTheme} text={`Reset`} />
+      <Text style={themed($title)} preset="heading">Settings</Text>
 
       <View style={themed($itemsContainer)}>
-        <Button onPress={toggleTheme} text={`Toggle Theme: ${themeContext}`} />
+        <Button style={themed($button)} onPress={toggleTheme} text={`Toggle Theme: ${themeContext}`} />
       </View>
-        <ListItem
-          LeftComponent={
-            <View style={themed($item)}>
-              <Text preset="bold">App Version</Text>
-              <Text>{Application.nativeApplicationVersion}</Text>
-            </View>
-          }
-        />
       <View style={themed($buttonContainer)}>
         <Button style={themed($button)} tx="common:logOut" onPress={logout} />
       </View>
-      
+      <View style={themed($footerContainer)}>
+        <Text>{Application.applicationName} : {Application.nativeApplicationVersion}</Text>
+      </View>
     </Screen>
   )
 }
@@ -113,17 +98,10 @@ const $container: ThemedStyle<ViewStyle> = ({ spacing }) => ({
 
 const $title: ThemedStyle<TextStyle> = ({ spacing }) => ({
   marginBottom: spacing.xxl,
+  textAlign: 'center',
 })
 
-const $reportBugsLink: ThemedStyle<TextStyle> = ({ colors, spacing }) => ({
-  color: colors.tint,
-  marginBottom: spacing.lg,
-  alignSelf: isRTL ? "flex-start" : "flex-end",
-})
 
-const $item: ThemedStyle<ViewStyle> = ({ spacing }) => ({
-  marginBottom: spacing.md,
-})
 
 const $itemsContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
   marginVertical: spacing.xl,
@@ -131,15 +109,14 @@ const $itemsContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
 
 const $button: ThemedStyle<ViewStyle> = ({ spacing }) => ({
   marginBottom: spacing.xs,
+  borderRadius: spacing.md,
 })
 
 const $buttonContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
   marginBottom: spacing.md,
 })
 
-const $hint: ThemedStyle<TextStyle> = ({ colors, spacing }) => ({
-  color: colors.palette.neutral600,
-  fontSize: 12,
-  lineHeight: 15,
-  paddingBottom: spacing.lg,
+const $footerContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  marginTop: spacing.xxxl,
+  alignItems: "center",
 })
